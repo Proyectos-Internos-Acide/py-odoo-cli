69f415dfe354b6278908b5b09f45099f87adc287
rogerinfas <rinfas@ulasalle.edu.pe>
2026-02-06 13:25:34 -0500

    feat: add custom exceptions to __init__.py for enhanced error handling

--- odoo_cli/__init__.py
+++ odoo_cli/__init__.py
@@ -1,1 +1,17 @@
 from .client import OdooClient
+from .exceptions import (
+    OdooClientError,
+    OdooConfigError,
+    OdooConnectionError,
+    OdooExecutionError,
+    OdooFaultError,
+)
+
+__all__ = [
+    "OdooClient",
+    "OdooClientError",
+    "OdooConfigError",
+    "OdooConnectionError",
+    "OdooExecutionError",
+    "OdooFaultError",
+]
